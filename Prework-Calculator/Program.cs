73a7332c832c5706247ae8f2dd43635adb907a68
Danul De Leon <danul.deleon@gmail.com>
2018-10-08 11:35:49 -0700

    Refactoring for user to choose method

--- Prework-Calculator/Program.cs
+++ Prework-Calculator/Program.cs
@@ -6,13 +6,19 @@ namespace Prework_Calculator
     {
         static void Main(string[] args)
         {
+            //User input to select which method to use.
+            Console.Write("Type number to choose method - (1) Add, (2) Subtract, (3) Multiply, (4) Divide: ");
+            string method = Console.ReadLine();
+            int methodToUse = int.Parse(method);
+
             //Add function enter inputs
-            Console.Write("Please enter 1st input for Add function: ");
-            string addFunctionInput1 = Console.ReadLine();
-            int addFunctionArg1 = int.Parse(addFunctionInput1);
-            Console.Write("Please enter 2nd input for Add function: ");
-            string addFunctionInput2 = Console.ReadLine();
-            int addFunctionArg2 = int.Parse(addFunctionInput2);
+            Console.Write("Please enter 1st input: ");
+            string functionInput1 = Console.ReadLine();
+            int addFunctionArg1 = int.Parse(functionInput1);
+            Console.Write("Please enter 2nd input: ");
+            string functionInput2 = Console.ReadLine();
+            int addFunctionArg2 = int.Parse(functionInput2);
+
             //invoking Add function using user inputs
             Console.WriteLine($"The sum of those two numbers is: {Add(addFunctionArg1, addFunctionArg2)}");
 
